934f83fa041e87795101ac8255557f94d044fb2a
Pontus A <pontus.abrahamsson@iths.se>
2023-11-13 15:06:39 +0100

    Configure parts of compose.yaml & Dockerfile

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/test", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/name", (req, res) => {
+  res.json(req.body.name);
 });
 
 app.listen(port, () => {
