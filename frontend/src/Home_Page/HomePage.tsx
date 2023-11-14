cc5d47a5f8360f2361fcb3d401b4ca41840186ba
Pontus A <pontus.abrahamsson@iths.se>
2023-11-14 21:13:30 +0100

    merge

---
+++ frontend/src/Home_Page/HomePage.tsx
@@ -0,0 +1,11 @@
+import "./css/Layout.css";
+import "./css/HomePage.css";
+import { Layout } from "./Layout";
+
+export const HomePage = () => {
+  return (
+    <div id="container-HomePage">
+      <Layout />
+    </div>
+  );
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./Home_Page/HomePage.tsx";
 
 const router = createBrowserRouter([
   {
@@ -9,8 +10,12 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "login",
-        element: <h1>lololol</h1>,
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/user",
+        element: <h1>User</h1>,
       },
     ],
   },
