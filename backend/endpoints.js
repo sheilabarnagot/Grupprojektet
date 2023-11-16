c5f50b60649f94b1fab32f80d311e8e72b7968d5
Pontus A <pontus.abrahamsson@iths.se>
2023-11-16 10:11:02 +0100

    Fix typo in endpoints and export/import query object

--- backend/endpoints.js
+++ backend/endpoints.js
@@ -1,29 +1,9 @@
 const router = require("express").Router();
 const { Client } = require("pg");
 const dotenv = require("dotenv");
+const query = require("./queries");
 
 dotenv.config();
-
-const query = {
-  users: {
-    text: "select _ from users;",
-  },
-  createforumposts: {
-    text: `INSERT INTO posts (userid, title, postcontent, topic ) VALUES ($1, $2, $3, $4) RETURNING _;`,
-  },
-  posts: {
-    text: "select * from posts;",
-  },
-  usercomment: {
-    text: `SELECT users.username, posts.postcontent, comments.commentcontent FROM users
-JOIN posts ON users.userid = posts.userid
-JOIN comments ON posts.postid = comments.postid
-JOIN UserComment ON users.userid = UserComment.userid
-AND comments.commentid = UserComment.CommentID
-WHERE posts.postid = $1
-AND users.userid = $2;`,
-  },
-};
 
 const client = new Client({
   connectionString: process.env.CONNECTION_STRING_LOCAL,
@@ -49,7 +29,7 @@ router.post("/createforumpost", async (req, res) => {
   const response = await client.query(query.createforumposts, [
     1,
     req.body.title,
-    req.body.postcomment,
+    req.body.postcontent,
     req.body.topic,
   ]);
 
---
+++ backend/queries.js
@@ -0,0 +1,22 @@
+const query = {
+  users: {
+    text: "select _ from users;",
+  },
+  createforumposts: {
+    text: `INSERT INTO posts (userid, title, postcontent, topic ) VALUES ($1, $2, $3, $4) RETURNING *;`,
+  },
+  posts: {
+    text: "select * from posts;",
+  },
+  usercomment: {
+    text: `SELECT users.username, posts.postcontent, comments.commentcontent FROM users
+JOIN posts ON users.userid = posts.userid
+JOIN comments ON posts.postid = comments.postid
+JOIN UserComment ON users.userid = UserComment.userid
+AND comments.commentid = UserComment.CommentID
+WHERE posts.postid = $1
+AND users.userid = $2;`,
+  },
+};
+
+module.exports = query;
