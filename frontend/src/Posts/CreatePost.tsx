db8b802cd6af10da1d5d010fbed59df5065f4758
Pontus A <pontus.abrahamsson@iths.se>
2023-11-17 17:38:24 +0100

    Test verification

--- frontend/src/Posts/CreatePost.tsx
+++ frontend/src/Posts/CreatePost.tsx
@@ -1,7 +1,7 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
-
+// yest
 export const CreatePost = () => {
   const [isSent, setIsSent] = useState(false);
   const [validation, setValidation] = useState(false);
