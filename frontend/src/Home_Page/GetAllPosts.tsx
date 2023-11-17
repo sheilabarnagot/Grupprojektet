33cd179be20cf9208f75905c46964bc1ea4aa89c
Pontus A <pontus.abrahamsson@iths.se>
2023-11-17 09:50:02 +0100

    Key prop

--- frontend/src/Home_Page/GetAllPosts.tsx
+++ frontend/src/Home_Page/GetAllPosts.tsx
@@ -37,18 +37,18 @@ export const GetAllPosts = ({ posts }: Post) => {
                   post.postcontent.split(' ').slice(0, 3).join(' ');
                 console.log(post.postid);
                 return (
-                  <>
-                    <div key={post.postid}>
-                      <NavLink href={`/posts/${post.userid}/${post.postid}`}>
-                        <div className="flex items-center">
-                          <p className="m-0 text-3xl pr-3">{post.title}</p>
-                          <p>By: {post.username}</p>
-                        </div>
-                        <p className="underline mb-3">{post.topic}</p>
-                        <p className="mb-3">{ingress}...</p>
-                      </NavLink>
-                    </div>
-                  </>
+                  <div key={post.postid}>
+                    {' '}
+                    {/* Add key prop */}
+                    <NavLink href={`/posts/${post.userid}/${post.postid}`}>
+                      <div className="flex items-center">
+                        <p className="m-0 text-3xl pr-3">{post.title}</p>
+                        <p>By: {post.username}</p>
+                      </div>
+                      <p className="underline mb-3">{post.topic}</p>
+                      <p className="mb-3">{ingress}...</p>
+                    </NavLink>
+                  </div>
                 );
               })}
         </div>
