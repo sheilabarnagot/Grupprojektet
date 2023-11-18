8bf19b6fc0d2672e464c7185e9eaa583fa4a830d
Sheila <sheila.martinezdiez@gmail.com>
2023-11-18 19:13:09 +0100

    Created login side

--- frontend/src/auth/pages/LoginPage.tsx
+++ frontend/src/auth/pages/LoginPage.tsx
@@ -1,12 +1,19 @@
-// import LoginForm from "../components/LoginForm";
+import LoginForm from "../components/LoginForm";
+import { Container, Row, Col } from "react-bootstrap";
+import "./LoginStyled.css"
 
-// const LoginPage: React.FC = () => {
-//   return (
-//     <div>
-//       <h1>Login Page</h1>
-//       <LoginForm />
-//     </div>
-//   );
-// };
 
-// export default LoginPage;
+const LoginPage: React.FC = () => {
+  return (
+    <Container>
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} md={6}>
+          <h2 className="text-center mb-4">Login</h2>
+          <LoginForm />
+        </Col>
+      </Row>
+    </Container>
+    );
+};
+
+export default LoginPage;
