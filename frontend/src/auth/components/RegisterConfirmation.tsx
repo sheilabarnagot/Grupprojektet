58a646ff099d82b3c208f41879b0711b7561b80e
Pontus A <pontus.abrahamsson@iths.se>
2023-11-30 10:53:54 +0100

    Add accept terms of use modal

---
+++ frontend/src/auth/components/RegisterConfirmation.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
+interface Props {
+  termsAccepted: boolean;
+  setTermsAccepted: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const RegisterConfirmation = ({ termsAccepted, setTermsAccepted }: Props) => {
+  const [show, setShow] = useState(true);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleUserTerms = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Submitted:', termsAccepted);
+    handleClose();
+  };
+
+  return (
+    <>
+      <Modal show={show}>
+        <div className="bg-slate-600 rounded-md">
+          <Modal.Header closeButton>
+            <Modal.Title className="text-black">
+              Acceptera användarvillkor
+            </Modal.Title>
+          </Modal.Header>
+          <Form onSubmit={handleUserTerms}>
+            <Modal.Body>
+              {' '}
+              <p className="mb-4">
+                Välkommen till vår webbapplikation! För att säkerställa din
+                integritet och ge dig en bättre användarupplevelse använder vi
+                cookies och behandlar vissa personuppgifter. Vi värnar om din
+                integritet och strävar efter att vara transparenta gällande hur
+                vi samlar in, använder och skyddar dina uppgifter.
+              </p>
+              <p>
+                För att använda våra tjänster behöver du acceptera vår
+                integritetspolicy. Den hittar du på sidan om
+                <NavLink className="ml-2" to="/gdpr">
+                  <span className="underline text-blue-400">
+                    integritet och cookies
+                  </span>
+                </NavLink>
+              </p>
+              <Form.Check
+                onClick={e => {
+                  const target = e.target as HTMLFormElement;
+                  setTermsAccepted(
+                    target.checkValidity() && !target.valueMissing
+                  );
+                }}
+                className="mt-4"
+                type="switch"
+                required
+                id="custom-switch"
+                label="Acceptera användarvillkor"
+              />
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button
+                disabled={!termsAccepted}
+                type="submit"
+                value="save changes">
+                Accept
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default RegisterConfirmation;
--- frontend/src/auth/components/RegisterForm.tsx
+++ frontend/src/auth/components/RegisterForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import RegisterConfirmation from './RegisterConfirmation';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
@@ -11,7 +12,7 @@ const RegisterForm: React.FC = () => {
     email: '',
     password: '',
   });
-
+  const [termsAccepted, setTermsAccepted] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,66 +37,76 @@ const RegisterForm: React.FC = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="firstName">
-        <Form.Label>First Name</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter your first name"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleChange}
-          required
-        />
-      </Form.Group>
-      <Form.Group controlId="lastName">
-        <Form.Label>Last Name</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter your last name"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-          required
-        />
-      </Form.Group>
-      <Form.Group controlId="userName">
-        <Form.Label>User Name</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Writte an user name"
-          name="userName"
-          value={formData.userName}
-          onChange={handleChange}
-          required
-        />
-      </Form.Group>
-      <Form.Group controlId="email">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter your email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </Form.Group>
-      <Form.Group controlId="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Enter your password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit" className="mt-4">
-        Submit
-      </Button>
-    </Form>
+    <div className="h-screen">
+      <RegisterConfirmation
+        termsAccepted={termsAccepted}
+        setTermsAccepted={setTermsAccepted}
+      />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="firstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your first name"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="lastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your last name"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="userName">
+          <Form.Label>User Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Writte an user name"
+            name="userName"
+            value={formData.userName}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter your email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter your password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Button
+          disabled={!termsAccepted}
+          variant="primary"
+          type="submit"
+          className="mt-4">
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 };
 
